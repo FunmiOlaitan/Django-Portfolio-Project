b009e467593f5d187e2d6e2ddcf9d11eb331c36c
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2024-04-03 20:03:49 +0100

    Update views to render HTML templates

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is my homepage (/)")
+    #return HttpResponse("This is my homepage (/)")
+    return render(request, 'home.html')
+
 def about(request):
-    return HttpResponse("This is my homepage (/about)")
+    #return HttpResponse("This is my homepage (/about)")
+    return render(request, 'about.html')
+
 def projects(request):
-    return HttpResponse("This is my homepage (/project)")
+    #return HttpResponse("This is my homepage (/project)")
+    return render(request, 'projects.html')
+
 def contact(request):
-    return HttpResponse("This is my homepage (/contacts)")
+    #return HttpResponse("This is my homepage (/contacts)")
+    return render(request, 'contact.html')
