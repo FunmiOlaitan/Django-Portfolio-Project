94f8fef0af123939a395b559224cd0272a3b2996
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2024-04-03 18:32:25 +0100

    Add basic views for homepage, about, projects, and contact pages

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("This is my homepage (/)")
+def about(request):
+    return HttpResponse("This is my homepage (/about)")
+def projects(request):
+    return HttpResponse("This is my homepage (/project)")
+def contact(request):
+    return HttpResponse("This is my homepage (/contacts)")
